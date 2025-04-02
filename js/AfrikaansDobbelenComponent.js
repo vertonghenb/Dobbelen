14650b335f1c5bf28dad0e0b3eef909fa0b48d10
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2025-04-02 14:51:41 +0200

    Afwerking

--- js/AfrikaansDobbelenComponent.js
+++ js/AfrikaansDobbelenComponent.js
@@ -1,26 +1,61 @@
 import Speler from "./Speler.js";
+import Spel from './Spel.js';
 
 export default class AfrikaansDobbelen {
-    #speler;
+    #spel;
+
+
+    #geefSpelers() {
+        const aantal = prompt('Geef het aantal spelers');
+        const spelers = [];
+
+        for (let index = 1; index <= aantal; index++) {
+            const naam = prompt(`naam van speler ${index}`);
+            spelers.push(naam);
+        }
+        return spelers;
+    }
 
     constructor() {
-        this.#speler = new Speler('Kirikou')
+        this.#spel = new Spel(this.#geefSpelers());
 
         document.getElementById('play').onclick = () => {
-            this.#speler.speel();
+            this.#spel.speel();
             this.#toHtml();
+        }
+        document.getElementById('scorebord').onclick = () => {
+            alert(this.#spel.scoreOverzicht);
         }
         this.#toHtml();
     }
 
     #toHtml() {
-        const { naam, score, dobbelstenen } = this.#speler;
+        const { naam, score, dobbelstenen } = this.#spel.spelerAanZet;
         for (let index = 0; index < Speler.aantalDobbelstenen; index++) {
             const aantal = dobbelstenen[index].aantalOgen;
             document.getElementById(index + 1).src = `images/Dice${aantal}.png`;
         }
-        document.getElementById('speler').innerText = `Naam: ${naam}`
+        document.getElementById('speler').innerText = `Speler aan zet: ${naam}`
         document.getElementById('score').innerText = `Score: ${score}`
+
+        if (this.#spel.heeftWinaar) {
+            alert(`${naam} u heeft gewonnen!`);
+            document.getElementById('play').disabled = true;
+        } else {
+            if (document.getElementById("play").value === "Rol dobbelstenen") {
+                document.getElementById("play").value = "Volgende speler";
+                document.getElementById("play").onclick = () => {
+                    this.#spel.bepaalVolgendeSpeler();
+                    this.#toHtml();
+                };
+            } else {
+                document.getElementById("play").value = "Rol dobbelstenen";
+                document.getElementById("play").onclick = () => {
+                    this.#spel.speel();
+                    this.#toHtml();
+                };
+            }
+        }
     }
 }
 
@@ -46,16 +81,3 @@ export default class AfrikaansDobbelen {
 /***************************************************************************************** */
 /* onderstaand stukje code heb je pas in de laatste stap van de oefening nodig (zie opgave) */
 /***************************************************************************************** */
-// if (document.getElementById("play").value === "Rol dobbelstenen") {
-//   document.getElementById("play").value = "Volgende speler";
-//   document.getElementById("play").onclick = () => {
-//     this.#spel.bepaalVolgendeSpeler();
-//     this.toHtml();
-//   };
-// } else {
-//   document.getElementById("play").value = "Rol dobbelstenen";
-//   document.getElementById("play").onclick = () => {
-//     this.#spel.speel();
-//     this.toHtml();
-//   };
-// }
